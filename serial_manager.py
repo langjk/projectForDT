325ab5548fd2a188bb45ce48cc5581a652b55fd1
langjk <langjinchi0515@126.com>
2025-01-17 17:01:54 +0800

    似乎不卡了

--- serial_manager.py
+++ serial_manager.py
@@ -1,38 +1,43 @@
-# serial_manager.py
 import serial
+from threading import Lock
 
 class SerialManager:
     def __init__(self):
         self.serial_port = None
         self.is_connected = False
+        self.lock = Lock()  # 添加锁
 
     def connect(self, port, baudrate=9600):
-        if self.is_connected:
-            return False  # 如果已经连接，返回False
-
-        self.serial_port = serial.Serial(port, baudrate, timeout=0.2)
-        if self.serial_port.is_open:
-            self.is_connected = True
-            return True
-        return False
+        with self.lock:  # 使用锁保护代码块
+            if self.is_connected:
+                return False  # 如果已经连接，返回False
+            self.serial_port = serial.Serial(port, baudrate, timeout=0.2)
+            if self.serial_port.is_open:
+                self.is_connected = True
+                return True
+            return False
 
     def disconnect(self):
-        if self.is_connected:
-            self.serial_port.close()
-            self.is_connected = False
-            return True
-        return False
+        with self.lock:  # 使用锁保护代码块
+            if self.is_connected:
+                self.serial_port.close()
+                self.is_connected = False
+                return True
+            return False
 
     def get_connection_status(self):
-        return self.is_connected  # 返回连接状态
+        with self.lock:  # 使用锁保护
+            return self.is_connected  # 返回连接状态
 
     def send_data(self, data):
-        if self.is_connected:
-            self.serial_port.write(data)
-            return True
-        return False
+        with self.lock:  # 使用锁保护
+            if self.is_connected:
+                self.serial_port.write(data)
+                return True
+            return False
 
     def receive_data(self, num_bytes):
-        if self.is_connected:
-            return self.serial_port.read(num_bytes)
-        return None
+        with self.lock:  # 使用锁保护
+            if self.is_connected:
+                return self.serial_port.read(num_bytes)
+            return None
